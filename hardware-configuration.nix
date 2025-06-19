2eba66f27c47176f7a87444f9b496564e1375f42
joek-dartist <dnlkrajewski@gmail.com>
2025-06-19 19:55:01 +0200

    Update hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -9,38 +9,14 @@
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/1ed54239-8e4d-4f6f-a869-69e258cc5f50";
-    fsType = "ext4"; # lub btrfs/xfs/zfs — zależnie od Twojego dysku
+    fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-  device = "/dev/disk/by-uuid/DE82-F39C";
-  fsType = "vfat";
-};
-
-  swapDevices = [ ]; # dodaj swap jeśli chcesz
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-
-  # NVIDIA specyficzne (w połączeniu z nvidia w configuration.nix)
-  {
-  hardware.opengl.enable = true;
-
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    modesetting.enable = true;
-    open = false;                            # ❗ Zamknięty sterownik
-    acceptLicense = true;                   # ✅ Automatyczna akceptacja licencji
-    powerManagement.enable = false;         # (opcjonalnie) zasilanie GPU
+    device = "/dev/disk/by-uuid/DE82-F39C";
+    fsType = "vfat";
   };
 
-
-  # Jeśli masz Wi-Fi przez kartę Intel/Realtek itp.
-  hardware.enableRedistributableFirmware = true;
-  hardware.firmware = [ pkgs.firmwareLinuxNonfree ];
-
-  # Opcjonalnie: ustaw DPI dla Twojego monitora (ultrapanorama)
-  services.xserver.dpi = 110;
+  swapDevices = [ ];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
