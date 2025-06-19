c1c199e3aaa2cee4b40f06240579b1e09e9b9fc3
joek-dartist <dnlkrajewski@gmail.com>
2025-06-19 19:27:05 +0200

    Update artstack.nix

--- artstack.nix
+++ artstack.nix
@@ -2,30 +2,18 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # Artystyczne narzędzia
     krita blender gimp inkscape mypaint
-
-    # Menedżer plików i terminal
     dolphin alacritty rofi
-
-    # Kompozytor i styl
-    picom kvantum qt5ct qt6ct
-
-    # Media i dodatki
-    vlc ffmpeg imagemagick
-
-    # Narzędzia systemowe
+    picom kvantum qt5ct qt6ct lxappearance
+    ffmpeg vlc imagemagick
     git neofetch nano unzip
     nvidia-smi nvidia-settings
     pavucontrol helvum qjackctl
-    # Twoje aliasy mogą działać przez `~/.bashrc`
-    alias fogaudio="pavucontrol & helvum &"
-    ];
+  ];
 
-  # Utwórz katalog ~/ART na start
   system.activationScripts.createArtFolder = {
     text = ''
-      mkdir -p /home/joek/ART/projekty /home/joek/ART/eksporty /home/joek/ART/palety
+      mkdir -p /home/joek/ART/{projekty,eksporty,palety}
       chown -R joek:users /home/joek/ART
     '';
   };
